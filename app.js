0f51f23dab8a08b7bded66a55510523593b39f62
Johnny77585 <jkuo2188@gmail.com>
2022-07-28 15:16:38 +0800

    create database & add seeders

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
