8a4d42e09310930ac5a355608f6139888e659966
Johnny77585 <jkuo2188@gmail.com>
2022-09-06 21:19:08 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.set('view engine', 'handlebars')
 
 app.use(session({
   secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
+  resave: false, //當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true //強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
